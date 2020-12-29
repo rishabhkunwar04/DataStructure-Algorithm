3d509940297685d5f1f9900dcd669ded57a194b1
Rishabh Kunwar <rishabh.kunwar04@gmail.com>
2020-12-29 17:46:25 +0530

    ..
    Signed-off-by: Rishabh Kunwar <rishabh.kunwar04@gmail.com>

---
+++ practice_sublime.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    //code
+    int n =10;
+    for(int i=1;i<=n;i++)
+    {
+        cout<<i;
+        if(i)
+        {
+            i++;
+        }
+    }
+    
+    return 0;
+}
