d89cd4b89aff6b06ed0bdfe84af4b0cf358df78e
Rishabh Kunwar <rishabh.kunwar04@gmail.com>
2021-08-29 14:45:14 +0530

    4th commit
    Signed-off-by: Rishabh Kunwar <rishabh.kunwar04@gmail.com>

--- characterarray.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-	char ch[]={'a','b','c','\0'};
-	cout<<ch<<endl;
-	return 0;
-}
--- practice.cpp
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-   // void foo();
-    //printf(""1"");
-        printf("1");
-
-}
